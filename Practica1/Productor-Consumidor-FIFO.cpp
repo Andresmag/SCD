3a79a2b0b73ed916602e210a7e03c9d7a3c100ff
Andres Molina <andresmag2@gmail.com>
2016-01-16 18:14:54 +0100

    Create Productor-Consumidor-FIFO.cpp

---
+++ Practica1/Productor-Consumidor-FIFO.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <cassert>
+#include <pthread.h>
+#include <semaphore.h>
+
+using namespace std ;
+
+// ---------------------------------------------------------------------
+// constantes 
+const unsigned 
+  num_items  = 40 ,
+  tam_vector = 10 ;     
+  
+//Vector que guarda los numeros producidos y su controlador
+int vector_datos[tam_vector];
+int primera_ocupada = 0;
+int primera_libre = 0;  
+
+//Semaforos de control
+sem_t
+	puede_producir,
+	puede_consumir,
+	mutex;       
+
+// ---------------------------------------------------------------------
+
+unsigned producir_dato()
+{
+  static int contador = 0 ;
+  sem_wait( &mutex);
+  cout << "producido: " << contador << endl << flush ;	//Muestra antes de aumentar
+  sem_post( &mutex);
+  return ++contador;
+}
+// ---------------------------------------------------------------------
+
+void consumir_dato( int dato )
+{
+    cout << "dato recibido: " << dato << endl ;
+}
+// ---------------------------------------------------------------------
+
+void * productor( void * )
+{  
+	int dato;
+	for( unsigned i = 0 ; i < num_items ; i=i+10 ){ 
+	
+		sem_wait( &puede_producir);	//Da la señal de que se puede producir
+		
+		for( int i=0; i < tam_vector;i++){
+    		dato = producir_dato() ;
+    		vector_datos[primera_libre] = dato;
+   		primera_libre++;
+   	}
+   	
+   	primera_libre %= 10;		//Lo reiniciamos
+   	
+   	//Una vez relleno el vector se da la señal de que se puede consumir
+    	sem_post( &puede_consumir);
+  }
+  return NULL ;
+}
+// ---------------------------------------------------------------------
+
+void * consumidor( void * )
+{  
+	int dato;
+	for( unsigned i = 0 ; i < num_items ; i=i+10 ){   
+    
+    sem_wait( &puede_consumir);	//Da la señal de que se puede consumir
+    for( int i=0; i < tam_vector; i++){
+    	dato = vector_datos[primera_ocupada];
+    	primera_ocupada++;
+    	sem_wait( &mutex);
+    	consumir_dato( dato );
+    	sem_post( &mutex);
+    }
+    
+    primera_ocupada %= 10;		//Lo reiniciamos
+    
+    //Una vez vacio el vector se puede seguir produciendo y guardando
+    sem_post( &puede_producir);
+    
+  }
+  return NULL ;
+}
+//----------------------------------------------------------------------
+
+int main()
+{
+   pthread_t hebra_productora, hebra_consumidora;
+   
+   sem_init( &mutex, 0, 1);
+   sem_init( &puede_producir, 0, 1);
+   sem_init( &puede_consumir, 0, 0);
+
+	pthread_create( &hebra_productora, NULL, productor, NULL);
+	pthread_create( &hebra_consumidora, NULL, consumidor, NULL);
+	
+	pthread_join( hebra_productora, NULL);
+	pthread_join( hebra_consumidora, NULL);
+
+	sem_destroy( &puede_producir);
+	sem_destroy( &puede_consumir);
+	sem_destroy( &mutex);
+	
+	cout << "\nFIN!\n\n";
+	 
+   return 0 ; 
+}
